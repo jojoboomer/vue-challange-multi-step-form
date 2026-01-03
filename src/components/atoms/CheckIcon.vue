26be1fde7fd2221287b6a685bdc938848d8d875d
jjlabrada <jjlabrada@xetid.cu>
2026-01-03 18:05:25 -0500

    Add check icon as component

---
+++ src/components/atoms/CheckIcon.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+type Props = {
+  color?: string
+  width?: string
+  height?: string
+  class?: string
+}
+const props = defineProps<Props>()
+
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="width ? width : 12" :height="height ? height : 9" viewBox="0 0 12 9"
+    :class="class">
+    <path fill="none" :stroke="color ? color : '#FFF'" stroke-width="2" d="m1 4 3.433 3.433L10.866 1" />
+  </svg>
+</template>
